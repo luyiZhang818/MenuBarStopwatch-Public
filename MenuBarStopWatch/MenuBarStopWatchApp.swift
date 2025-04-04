16e53efc3c4b8700e87f2ee9fbfef65592b9a7b5
Luyi Zhang <iyulgnahz@outlook.com>
2025-04-04 10:46:39 -0400

    Initial Commit

---
+++ MenuBarStopWatch/MenuBarStopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarStopWatchApp.swift
+//  MenuBarStopWatch
+//
+//  Created by Luyi Zhang on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarStopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MenuBarStopWatchTests/MenuBarStopWatchTests.swift
@@ -0,0 +1,16 @@
+//
+//  MenuBarStopWatchTests.swift
+//  MenuBarStopWatchTests
+//
+//  Created by Luyi Zhang on 4/4/25.
+//
+
+import Testing
+
+struct MenuBarStopWatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
